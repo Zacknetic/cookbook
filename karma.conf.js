eb2278d847e58babafcc0c728d70b005e080d886
Zacknetic <zackavino@gmail.com>
2024-08-09 16:48:05 -0400

    update to use firefox headless

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Firefox'],
+    browsers: ['Firefox', 'FirefoxHeadless'],
     restartOnFileChange: true
   });
 };
